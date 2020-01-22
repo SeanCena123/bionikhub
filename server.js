e855e286dbacdd06e523486a1a40809dc2ff1a8c
Defonse <6430@studentsjc.com.au>
2020-01-22 15:15:53 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var app = require("express")(); 
+var bodyParser = require("body-parser"); 
+var user = "Prodicode";
+
+
+//Set view engine to ejs
+app.set("view engine", "ejs"); 
+
+//Tell Express where we keep our index.ejs
+app.set("views", __dirname + "/views"); 
+
+//Use body-parser
+app.use(bodyParser.urlencoded({ extended: false })); 
+
+//Instead of sending Hello World, we render index.ejs
+app.get("/", (req, res) => { res.render("index", { username: user }); });
+
+app.listen(8000, () => { console.log("Server online on http://localhost:8000"); });
